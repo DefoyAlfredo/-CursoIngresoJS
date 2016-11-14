562c4822bdec18f1717258567ee6858ec882ce4b
Defoy Alfredo Damian <alfredodefoy@gmail.com>
2016-11-15 00:23:59 +0200

    Add files via upload

---
+++ 2-InstruccionIf/jsEntradaSalida-5.js
@@ -0,0 +1,17 @@
+/*Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
+ej.: "Usted se llama José y tiene 66 años" 	*/
+function Mostar()
+{	
+	//id="FormIngreso
+	//id="elNombre">
+	//id="laEdad"
+	var dat=prompt("ingrese Nombre");
+	var edad=prompt("ingrese edad");
+	document.getElementById('elNombre'). value =dat ;
+	document.getElementById('laEdad'). value =edad ;
+
+}
+
+
+
+
