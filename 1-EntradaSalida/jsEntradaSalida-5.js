8fd65daca9f7e49ffedea6a88dc703abcb71fe70
Defoy Alfredo Damian <alfredodefoy@gmail.com>
2016-11-15 00:50:56 +0200

    comit3

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -2,6 +2,16 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function Mostar()
 {	
-	
+	//id="FormIngreso
+	//id="elNombre">
+	//id="laEdad"
+	var dat=prompt("ingrese Nombre");
+	var edad=prompt("ingrese edad");
+	document.getElementById('elNombre'). value =dat ;
+	document.getElementById('laEdad'). value =edad ;
+
 }
+
+
+
 
