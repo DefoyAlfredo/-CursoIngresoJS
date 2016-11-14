8e1eefba78689119bf301f34864796105662c633
Defoy Alfredo Damian <alfredodefoy@gmail.com>
2016-11-15 00:01:51 +0200

    Add files via upload
    pruebas

---
+++ 2-InstruccionIf/jsEntradaSalida-2.js
@@ -0,0 +1,25 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
+function Mostar()
+{
+	/*
+	concepto de variable
+	concatenacion
+	prompt
+    simvolo  + concatena datos.
+
+
+
+	*/
+	var producto;
+	producto =  "samsung";
+	producto= prompt("ingrese marca", " sin marca");
+	var precio= ("ingrese precio", "999")
+
+
+
+
+	alert("su producto es: " + producto + " valor es: " + precio);
+
+}
+
