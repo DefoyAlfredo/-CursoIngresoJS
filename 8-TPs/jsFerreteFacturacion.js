2cd7ed53c5b725b6b8b944b684e370df49540847
Defoy Alfredo Damian <alfredodefoy@gmail.com>
2016-11-15 19:13:31 -0300

    pruebas de tps
    echo en clase jis

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,56 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
+	//id="PrecioUno"
+	//id="PrecioDos"
+	//id="PrecioTres"
 	
+
+	var PrecioUno;
+	var PrecioDos;
+	var PrecioTres;
+	PrecioUno=document.getElementById('PrecioUno').value;
+	PrecioDos=document.getElementById('PrecioDos').value;
+	PrecioTres=document.getElementById('PrecioTres').value;
+
+	PrecioUno=parseInt(PrecioUno);
+	PrecioDos=parseInt(PrecioDos);
+	PrecioTres=parseInt(PrecioTres);
+	var suma= PrecioUno+PrecioDos+PrecioTres;
+	alert("SUMA: "+ suma);
 }
 function Promedio () 
 {
-	
+	var PrecioUno;
+	var PrecioDos;
+	var PrecioTres;
+	PrecioUno=document.getElementById('PrecioUno').value;
+	PrecioDos=document.getElementById('PrecioDos').value;
+	PrecioTres=document.getElementById('PrecioTres').value;
+	PrecioUno=parseInt(PrecioUno);
+	PrecioDos=parseInt(PrecioDos);
+	PrecioTres=parseInt(PrecioTres);
+ 
+ 	var promedio=(PrecioUno+PrecioDos+PrecioTres) /3;
+ 	alert(promedio);
 }
 function PrecioFinal () 
 {
+	var PrecioUno;
+	var PrecioDos;
+	var PrecioTres;
+	PrecioUno=document.getElementById('PrecioUno').value;
+	PrecioDos=document.getElementById('PrecioDos').value;
+	PrecioTres=document.getElementById('PrecioTres').value;
+	PrecioUno=parseInt(PrecioUno);
+	PrecioDos=parseInt(PrecioDos);
+	PrecioTres=parseInt(PrecioTres);
+
+	var PrecioFinal=(PrecioUno+PrecioDos+PrecioTres)*1.21;
+	PrecioFinal=parseInt(PrecioFinal);
+	alert(PrecioFinal);
+	console.log("hola");
+	console.log(PrecioFinal);
+	console.log(PrecioUno);
 	
 }
