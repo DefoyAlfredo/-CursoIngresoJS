0b06be25423cf012a87ff3461992f4cec2149cc6
Defoy Alfredo Damian <alfredodefoy@gmail.com>
2016-12-05 16:16:36 -0300

    comit final
    trabajos realizados en clases

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -4,6 +4,30 @@ function Mostrar()
 var mesDelAño = document.getElementById('mes').value;
 
 
+switch(mesDelAño)
+{
+	case "Enero":
+	alert("que comience bien el año");
+	break;
 
+	case "Febrero":
+	alert()
+	case "Marzo":
+	alert("A Clases");
+		
+	break;
+	case "Julio":
+	alert("se vienen las vacaciones!!!");
+	break;
+		
+	break;
+	case "Diciembre":
+	alert("Felices Fiestas!!!");
+	break;
+	
+	default:
+	break;
+
+}
 
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -3,8 +3,18 @@ function Mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
+alert (mesDelAño);
 	
+switch(mesDelAño)
+{
+	case "Febrero":
+	alert("Este mes no tiene las de 29 dias");
+	break;
+	default:
+	alert("Este mes tiene 30 o mas");
+	break;
+
+}
 	
 
 
